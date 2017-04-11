7f59b935c2397a20065453993ff20e37467bcfd5
DL1992 <laadan1235@gmail.com>
2017-04-12 00:29:54 +0300

    Update RunMazeGenerator.java

--- src/test/RunMazeGenerator.java
+++ src/test/RunMazeGenerator.java
@@ -12,6 +12,7 @@ public class RunMazeGenerator {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             testMazeGenerator(new SimpleMazeGenerator());
+            //IM HERE!!!
         }
 //      testMazeGenerator(new MyMazeGenerator());
     }
