a49e9ff907de58b920482d5f594c5c4c6f4d3f0e
DoronLaadan <laadan1235@gmail.com>
2017-04-12 00:49:50 +0300

    checking commit

--- src/test/RunMazeGenerator.java
+++ src/test/RunMazeGenerator.java
@@ -11,7 +11,6 @@ import algorithms.mazeGenerators.SimpleMazeGenerator;
 public class RunMazeGenerator {
     public static void main(String[] args) {
         testMazeGenerator(new SimpleMazeGenerator());
-        //HEREEEE
 //      testMazeGenerator(new MyMazeGenerator());
     }
 
